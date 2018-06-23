39e4e4dc2b7a045a361126abf0bd6d2ddae8087e
emanuel.perez.astorga@gmail.com <emanuel.perez.astorga@gmail.com>
2018-06-22 22:01:51 -0400

    segundo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,21 @@
 class ProductsController < ApplicationController
 
   def create
-    @product = Product.new(product_params)
-    @product.save
-    redirect_to @product.category
+    product = Product.new(product_params)
+    product.category_id = params[:category_id]
+    product.save
+    redirect_to product.category
   end
 
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-    redirect_to show_path
+    redirect_to @product.category
 
   end
 
   private
   def product_params
-      params.require(:product).permit(:name, :price, :category_id)
+      params.require(:product).permit(:name, :price)
   end
 end
